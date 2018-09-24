23a76a0b73631afea2c44df65c0aecc546f686fe
daviddiazvico <david.diaz.vico@outlook.com>
2018-09-24 23:50:24 +0200

    Bug fixed in validation split strategy

--- sksacred/__init__.py
+++ sksacred/__init__.py
@@ -1,13 +1,10 @@
 """
-Scikit-learn-compatible Sacred experiments.
-
 @author: David Diaz Vico
-@license: MIT
 """
 
 import pickle
 from sacred import Experiment, Ingredient
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import BaseCrossValidator, cross_val_score
 import tempfile
 
 from dataset import select_dataset
@@ -26,19 +23,30 @@ def run(persist=False):
     """ Run an experiment. """
     X, y, X_test, y_test, inner_cv, outer_cv = select_dataset()
     estimator = select_estimator(X=X, y=y, cv=inner_cv)
-    if X_test is None:
-        score = cross_val_score(estimator, X, y, cv=outer_cv)
-        if persist:
-            estimator.fit(X, y)
-    else:
+    if (X_test is not None) and (y_test is not None):
+        # Test split for scoring
         estimator.fit(X, y)
         score = estimator.score(X_test, y_test)
+    else:
+        # CV scoring
+        if isinstance(inner_cv, BaseCrossValidator) or hasattr(inner_cv, '__iter__'):
+            # Validation split for hyperparameter tuning
+            estimator.fit(X, y)
+            score = cross_val_score(estimator.best_estimator_, X, y,
+                                    cv=outer_cv)
+        else:
+            # Random CV folds for hyperparameter tuning
+            score = cross_val_score(estimator, X, y, cv=outer_cv)
+        if persist:
+            estimator.fit(X, y)
+    # Record score and other attributes as experiment info
     experiment.info['score'] = score
     for attr in ['cv_results_', 'best_score_', 'best_params_', 'best_index_',
                  'n_splits_']:
         if hasattr(estimator, attr):
             experiment.info[attr] = estimator.__dict__[attr]
     if persist:
+        # Store the model
         handler = tempfile.NamedTemporaryFile('wb')
         pickle.dump(estimator, handler)
         experiment.add_artifact(handler.name, name='estimator.pkl')
