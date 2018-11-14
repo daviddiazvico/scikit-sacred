d6fc626aec64bad69c427d6fa1ed8cf11ef15282
daviddiazvico <david.diaz.vico@outlook.com>
2018-11-15 00:11:13 +0100

    Refactor

--- sksacred/__init__.py
+++ sksacred/__init__.py
@@ -1,53 +1,79 @@
 """
 @author: David Diaz Vico
+@license: MIT
 """
 
 import pickle
 from sacred import Experiment, Ingredient
-from sklearn.model_selection import BaseCrossValidator, cross_val_score
 import tempfile
 
-from dataset import select_dataset
-from estimator import select_estimator
 
+def experiment(fetch_dataset, initialize_estimator, cross_val_score,
+               attrs=None):
+    """Prepare a Scikit-learn experiment as a Sacred experiment.
 
-dataset = Ingredient('dataset')
-select_dataset = dataset.capture(select_dataset)
-estimator = Ingredient('estimator')
-select_estimator = estimator.capture(select_estimator)
-experiment = Experiment(ingredients=[dataset, estimator])
+    Prepare a Scikit-learn experiment indicating a dataset and an estimator and
+    return it as a Sacred experiment.
 
+    Parameters
+    ----------
+    fetch_dataset : function
+        Dataset fetch function. Might receive any argument. Must return X, y
+        (might be None), X_test (might be None), y_test (might be None),
+        inner_cv (might be None), outer_cv (might be None).
+    initialize_estimator : function
+        Estimator initialization function. Must receive at least X, y and cv,
+        all of which can be None, and might receive any other argument. Must
+        return an initialized sklearn-compatible estimator.
+    cross_val_score : function
+        Function to perform cross-validation scoring on the estimator. Must
+        receive the estimator, X, y and cv (migth be None). Must return the CV
+        score.
+    attrs : iterable, default=None
+        Tuple or list of names of estimator attributes to be saved as experiment
+        info, or None.
 
-@experiment.automain
-def run(persist=False):
-    """ Run an experiment. """
-    X, y, X_test, y_test, inner_cv, outer_cv = select_dataset()
-    estimator = select_estimator(X=X, y=y, cv=inner_cv)
-    if (X_test is not None) and (y_test is not None):
-        # Test split for scoring
-        estimator.fit(X, y)
-        score = estimator.score(X_test, y_test)
-    else:
-        # CV scoring
-        if isinstance(inner_cv, BaseCrossValidator) or hasattr(inner_cv, '__iter__'):
-            # Validation split for hyperparameter tuning
-            estimator.fit(X, y)
-            score = cross_val_score(estimator.best_estimator_, X, y,
-                                    cv=outer_cv)
+    Returns
+    -------
+    experiment : Experiment
+        Sacred experiment, ready to be run.
+
+    """
+
+    dataset = Ingredient('dataset')
+    fetch_dataset = dataset.capture(fetch_dataset)
+    estimator = Ingredient('estimator')
+    initialize_estimator = estimator.capture(initialize_estimator)
+    experiment = Experiment(ingredients=(dataset, estimator))
+
+    @experiment.automain
+    def run():
+        """Run the experiment.
+
+        Run the experiment.
+
+        Returns
+        -------
+        info : dictionary
+            Experiment info.
+
+        """
+        X, y, X_test, y_test, inner_cv, outer_cv = fetch_dataset()
+        estimator = initialize_estimator(X=X, y=y, cv=inner_cv)
+        if X_test is not None:
+            estimator.fit(X, y=y)
+            experiment.info['score'] = estimator.score(X_test, y=y_test)
         else:
-            # Random CV folds for hyperparameter tuning
-            score = cross_val_score(estimator, X, y, cv=outer_cv)
-        if persist:
-            estimator.fit(X, y)
-    # Record score and other attributes as experiment info
-    experiment.info['score'] = score
-    for attr in ['cv_results_', 'best_score_', 'best_params_', 'best_index_',
-                 'n_splits_']:
-        if hasattr(estimator, attr):
-            experiment.info[attr] = estimator.__dict__[attr]
-    if persist:
-        # Store the model
+            experiment.info['score'] = cross_val_score(estimator, X, y=y,
+                                                       cv=outer_cv)
+            estimator.fit(X, y=y)
+        if attrs is not None:
+            for attr in attrs:
+                if hasattr(estimator, attr):
+                    experiment.info[attr] = estimator.__dict__[attr]
         handler = tempfile.NamedTemporaryFile('wb')
         pickle.dump(estimator, handler)
         experiment.add_artifact(handler.name, name='estimator.pkl')
-    return score
+        return experiment.info
+
+    return experiment
--- tests/test.py
+++ tests/test.py
@@ -1,24 +1,33 @@
 """
-Tests.
-
 @author: David Diaz Vico
 @license: MIT
 """
 
-import os
-import sys
+from sklearn.datasets import load_iris
+from sklearn.model_selection import (cross_val_score, GridSearchCV,
+                                     train_test_split)
+from sklearn.tree import DecisionTreeClassifier
 
-sys.path.append(os.path.join(os.getcwd(), 'tests'))
 from sksacred import experiment
 
 
-def test_cv():
-    """Tests scikit-sacred."""
-    experiment.run(config_updates={'dataset': {'dataset': 'iris_cv'},
-                                   'estimator': {'predictor': 'LogisticRegression'}})
+def test_experiment():
+    """Tests experiment."""
 
+    def fetch_dataset(test=False):
+        X, y = load_iris(return_X_y=True)
+        X_test = y_test = None
+        inner_cv = outer_cv = 3
+        if test:
+            X, X_test, y, y_test = train_test_split(X, y)
+        return X, y, X_test, y_test, inner_cv, outer_cv
 
-def test_train_test():
-    """Tests scikit-sacred."""
-    experiment.run(config_updates={'dataset': {'dataset': 'iris_train_test'},
-                                   'estimator': {'predictor': 'SVC'}})
+    def initialize_estimator(X=None, y=None, cv=None):
+        return GridSearchCV(DecisionTreeClassifier(), {'max_depth': [2, 4, 8]},
+                            iid=True, cv=cv)
+
+    e = experiment(fetch_dataset, initialize_estimator, cross_val_score,
+                   attrs=['cv_results_', 'best_score_', 'best_params_',
+                          'best_index_', 'n_splits_'])
+    for test in (True, False):
+        e.run(config_updates={'dataset': {'test': test}})
