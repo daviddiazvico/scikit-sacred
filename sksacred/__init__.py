93ed9326acb224a2be536cc8dc2de27e1fc882c0
daviddiazvico <david.diaz.vico@outlook.com>
2018-11-20 00:23:18 +0100

    Refactor

--- sksacred/__init__.py
+++ sksacred/__init__.py
@@ -3,9 +3,7 @@
 @license: MIT
 """
 
-import pickle
 from sacred import Experiment, Ingredient
-import tempfile
 
 
 def experiment(dataset, estimator, cross_validate):
@@ -17,8 +15,9 @@ def experiment(dataset, estimator, cross_validate):
     Parameters
     ----------
     dataset : function
-        Dataset fetch function. Might receive any argument. Must return X, y
-        (might be None), inner_cv (might be None) and outer_cv (might be None).
+        Dataset fetch function. Might receive any argument. Must return a Bunch
+        with data, target (might be None), inner_cv (might be None) and outer_cv
+        (might be None).
     estimator : function
         Estimator initialization function. Must receive at least X, y and cv,
         all of which can be None, and might receive any other argument. Must
@@ -26,7 +25,8 @@ def experiment(dataset, estimator, cross_validate):
     cross_validate : function
         Function to evaluate metrics by cross-validation. Must receive the
         estimator, X, y (migth be None) and cv (migth be None). Must return a
-        dictionary with the cross-validation score and maybe other info.
+        dictionary with the cross-validation score and maybe other info, like
+        a list of fitted estimators.
 
     Returns
     -------
@@ -45,22 +45,10 @@ def experiment(dataset, estimator, cross_validate):
 
     @experiment.automain
     def run():
-        """Run the experiment.
-
-        Run the experiment.
-
-        Returns
-        -------
-        info : dictionary
-            Experiment scores and maybe other info.
-
-        """
-        X, y, inner_cv, outer_cv = dataset()
-        e = estimator(X=X, y=y, cv=inner_cv)
-        experiment.info.update(cross_validate(e, X, y=y, cv=outer_cv))
-        with tempfile.NamedTemporaryFile('wb') as handler:
-            pickle.dump(e.fit(X, y=y), handler)
-            experiment.add_artifact(handler.name, name='estimator.pkl')
-        return experiment.info
+        """Run the experiment."""
+        data = dataset()
+        e = estimator(X=data.data, y=data.target, cv=data.inner_cv)
+        scores = cross_validate(e, data.data, y=data.target, cv=data.outer_cv)
+        experiment.info.update(scores)
 
     return experiment
--- tests/test.py
+++ tests/test.py
@@ -3,6 +3,7 @@
 @license: MIT
 """
 
+from sacred.observers import FileStorageObserver
 from sklearn.datasets import load_iris
 from sklearn.model_selection import cross_validate, GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
@@ -14,12 +15,15 @@ def test_experiment():
     """Tests experiment."""
 
     def dataset(inner_cv=None, outer_cv=None):
-        X, y = load_iris(return_X_y=True)
-        return X, y, inner_cv, outer_cv
+        data = load_iris()
+        data.inner_cv = inner_cv
+        data.outer_cv = outer_cv
+        return data
 
     def estimator(X=None, y=None, cv=None):
         return GridSearchCV(DecisionTreeClassifier(), {'max_depth': [2, 4, 8]},
                             iid=True, cv=cv)
 
     e = experiment(dataset, estimator, cross_validate)
+    e.observers.append(FileStorageObserver.create('.results'))
     e.run(config_updates={'dataset': {'inner_cv': 3, 'outer_cv': 3}})
